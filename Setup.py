51518f0d0c59094c6c60431067b42276e6fd4d0f
Asif-AI <70411134+Asif-AI@users.noreply.github.com>
2022-04-14 13:17:08 +0530

    Initial Setup

---
+++ Setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup
+
+with open("README.md", "r", encoding="utf-8") as f:
+    long_description = f.read()
+
+
+REPO_NAME = "OpenCV_Basic"
+SRC_REPO = "src"
+AUTHOR_USER_NAME = "Asif-AI"
+LIST_OF_REQUIREMENTS = []
+
+
+setup(
+    name=SRC_REPO,
+    version="0.0.1",
+    author=AUTHOR_USER_NAME,
+    description="A small package for MLflow app",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url=f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}",
+    author_email="aasifkhan02@gmail.com",
+    packages=[SRC_REPO],
+    license="MIT",
+    python_requires=">=3.6",
+    install_requires=LIST_OF_REQUIREMENTS
+)
